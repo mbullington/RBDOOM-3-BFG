5eed2359545fd4afe14ac148bd3057ee31c1ca46
Michael Bullington <michael.bullington@mapbox.com>
2022-05-01 19:15:16 -0400

    for right now, the shaderc experiment failed as BFG shaders are not valid HLSL

--- install-deps-macos.sh
+++ install-deps-macos.sh
@@ -3,14 +3,5 @@
 # Install homebrew dependencies.
 brew install wget cmake sdl2 openal-soft ffmpeg ninja vulkan-headers molten-vk
 
-SHADERC_DIR=/opt/shaderc
-if [ ! -d "$SHADERC_DIR" ]; then
-    cd ~/Downloads
-    wget https://storage.googleapis.com/shaderc/artifacts/prod/graphics_shader_compiler/shaderc/macos/continuous_clang_release/388/20220202-124410/install.tgz
-    tar -xzvf install.tgz
-    mv install /opt/shaderc
-fi
-
 echo "NEXT STEPS"
-echo "Add /opt/shaderc/bin to your PATH"
 echo "Add to your ~/.zshrc: export MOLTENVK_SDK=$(find /usr/local/Cellar/molten-vk/* | head -n 1)"
