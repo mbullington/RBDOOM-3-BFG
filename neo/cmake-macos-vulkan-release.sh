4cde6adbe27dfff1014674d89b5a310289805b0d
Michael Bullington <michael.bullington@mapbox.com>
2022-05-01 18:27:05 -0400

    work towards removing direct support for shaderc (part 1/?)

--- neo/cmake-macos-vulkan-release.sh
+++ neo/cmake-macos-vulkan-release.sh
@@ -6,7 +6,7 @@ mkdir build
 cd build
 
 # change or remove -DCMAKE_OSX_DEPLOYMENT_TARGET=<version> to match supported runtime targets
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.12 -DCOMPILE_COMMANDS=ON -DFFMPEG=OFF -DBINKDEC=ON -DUSE_VULKAN=ON -DSPIRV_SHADERC=OFF -DUSE_MoltenVK=ON -DOPENAL_LIBRARY=/usr/local/opt/openal-soft/lib/libopenal.dylib -DOPENAL_INCLUDE_DIR=/usr/local/opt/openal-soft/include ../neo -Wno-dev
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.12 -DCOMPILE_COMMANDS=ON -DFFMPEG=OFF -DBINKDEC=ON -DUSE_VULKAN=ON -DUSE_MoltenVK=ON -DOPENAL_LIBRARY=/usr/local/opt/openal-soft/lib/libopenal.dylib -DOPENAL_INCLUDE_DIR=/usr/local/opt/openal-soft/include ../neo -Wno-dev
 
 # cp compile_commands.json to root of project so clangd can use it
 cp compile_commands.json ..
