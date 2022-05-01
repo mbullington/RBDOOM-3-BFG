22c632ebed56c885819e35935d8c652b10b41fe2
Michael Bullington <michael.bullington@mapbox.com>
2022-05-01 01:41:41 -0400

    enable clangd, build_vulkan -> build

--- neo/cmake-macos-opengl-retail.sh
+++ neo/cmake-macos-opengl-retail.sh
@@ -6,4 +6,7 @@ mkdir build
 cd build
 
 # change or remove -DCMAKE_OSX_DEPLOYMENT_TARGET=<version> to match supported runtime targets
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS_RELEASE="-DID_RETAIL" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.12 -DSDL2=ON -DFFMPEG=OFF -DBINKDEC=ON -DOPENAL_LIBRARY=/usr/local/opt/openal-soft/lib/libopenal.dylib -DOPENAL_INCLUDE_DIR=/usr/local/opt/openal-soft/include ../neo -Wno-dev
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS_RELEASE="-DID_RETAIL" -DCOMPILE_COMMANDS=ON -DCMAKE_OSX_DEPLOYMENT_TARGET=10.12 -DSDL2=ON -DFFMPEG=OFF -DBINKDEC=ON -DOPENAL_LIBRARY=/usr/local/opt/openal-soft/lib/libopenal.dylib -DOPENAL_INCLUDE_DIR=/usr/local/opt/openal-soft/include ../neo -Wno-dev
+
+# cp compile_commands.json to root of project so clangd can use it
+cp compile_commands.json ..
